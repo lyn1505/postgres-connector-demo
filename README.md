ceb95f154e6377aae6042e0c0b82dd70b4bd2241
nttly1505 <ly.nguyen@skymavis.com>
2024-10-15 15:25:18 +0700

    add instruction

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # postgres-connector-demo
+
+## To run the demo
+1. Clone the repository
+2. Run the databases. This will run `docker-compose up` under the hood, and set up 2 masters and 2 slaves:
+```bash
+make up master=1 slave=1 # can set different numbers to scale
+```
+
+3. Run the tests in [`db_test.go`](https://github.com/lyn1505/postgres-connector-demo/blob/main/db_test.go). The databases will log SQL queries it executes, so to see the logs, run any of these:
+```bash
+docker-compose logs -f pg-demo-postgresql-master-users-1
+docker-compose logs -f pg-demo-postgresql-master-games-1
+docker-compose logs -f pg-demo-postgresql-slave-users-1
+docker-compose logs -f pg-demo-postgresql-slave-games-1
+```
+Read the comments in the test file to understand what each test does.
+
+4. When you're done:
+```bash
+make clean # remove containers and volumes
+```
--- db_test.go
+++ db_test.go
@@ -31,6 +31,8 @@ func initDB() (*gorm2.DB, error) {
 	)
 }
 
+// TestWriteToGamesTable executes a write query to "games" table.
+// >> Watch for logs in "games" MASTER container to see that the query is executed there.
 func TestWriteToGamesTable(t *testing.T) {
 	db, err := initDB()
 	assert.NoError(t, err)
@@ -46,6 +48,9 @@ func TestWriteToGamesTable(t *testing.T) {
 	fmt.Printf("Write to games table: %+v\n", game)
 }
 
+// TestReadFromGamesTableWithPreload executes a 2 read queries to "games" and "users" table.
+// RUN TestWriteToGameTable FIRST before running this test.
+// >> Watch for logs in "users" SLAVE & "games" SLAVE container to see that the queries are executed there.
 func TestReadFromGamesTableWithPreload(t *testing.T) {
 	db, err := initDB()
 	assert.NoError(t, err)
@@ -66,6 +71,8 @@ func TestReadFromGamesTableWithPreload(t *testing.T) {
 	fmt.Printf("Read from games table with preload: %+v, admin:%+v\n", game, game.Admin)
 }
 
+// TestWriteToUsersTable executes a write query to "users" table.
+// >> Watch for logs in "users" MASTER container to see that the query is executed there.
 func TestWriteToUsersTable(t *testing.T) {
 	db, err := initDB()
 	assert.NoError(t, err)
@@ -80,6 +87,8 @@ func TestWriteToUsersTable(t *testing.T) {
 	fmt.Printf("Write to users table: %+v\n", user)
 }
 
+// TestReadFromUsersTable executes a read query to "users" table.
+// >> Watch for logs in "users" SLAVE container to see that the query is executed there.
 func TestReadFromUsersTable(t *testing.T) {
 	db, err := initDB()
 	assert.NoError(t, err)
